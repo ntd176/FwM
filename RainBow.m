1fe3c0b3ea9f87dcb53b25cf208cc151788daf37
ntd176 <tiendai17062003@gmail.com>
2025-04-27 21:42:44 +0700

    update fix

--- RainBow.m
+++ RainBow.m
@@ -1,11 +1,11 @@
-%setup parameters
+% setup parameters
 figure;
-%pause(5);
-t=0:0.001:pi;
-%draw radius from the outside to the inside
+% pause(5);
+t=0:0.01:pi;
+% outer, inner radius
 outer_radius = [8, 7, 6, 5, 4, 3, 2];
 inner_radius = [7, 6, 5, 4, 3, 2, 1];
-%color
+% color
 colors = {
     [1, 0, 0],                  %red
     [1, 0.5, 0],                %orange
@@ -13,9 +13,9 @@ colors = {
     [0, 1, 0],                  %green
     [0.5843, 0.8157, 0.9882],   %light blue
     [0, 0, 1],                  %blue
-    [0.4039, 0.0078, 0.3686]   %violet
+    [0.4039, 0.0078, 0.3686]    %violet
 }
-% %draw using the loop
+% draw using the loop
 hold on;
 
 for i = 1:length(outer_radius)
@@ -24,17 +24,18 @@ for i = 1:length(outer_radius)
     r_inner = inner_radius(i);
     color = colors{i};
 
-    %mathematical principle (polar coordinates to cartesian coordinates)
+    % mathematical principle (polar coordinates to cartesian coordinates)
     x_outer = r_outer * cos(t);
     y_outer = r_outer * sin(t);
 
     x_inner = r_inner * cos(t);
     y_inner = r_inner * sin(t);
-
+    
+    % reverse
     x_band = [x_outer, fliplr(x_inner)];
     y_band = [y_outer, fliplr(y_inner)];
 
-      
+    patch(x_band, y_band, color, 'EdgeColor', 'none');  
 end
 
 axis equal;
